24e223baf996621a8ab6a356d690de7985f0051f
surnik20 <165255503+surnik20@users.noreply.github.com>
2024-03-29 17:11:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,20 @@ variable "instancetype" {
   description = "Type of VM instance to provision"
   default     = "t2.micro"
 }
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "project" =  "test",
+    "subproject" = "test",
+    "env" = "dev",
+    "subenv" = "dev"
+  }
+}
+
+variable "aws" {
+  type = map(string)
+  vpcid = "vpc-054be7e773d4734fe"
+  amiid = "ami-075491ab3df78dd2d"
+  subnet = "subnet-0f7658858b7d17e6d"
+}
